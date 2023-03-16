3ff89a4f2b768a59c77fc12d30674ccb7e2be214
ohyajk <ohyajk@gmail.com>
2023-03-16 16:56:02 +0530

    Add factorial method

--- solver.rb
+++ solver.rb
@@ -4,6 +4,21 @@ class Solver
       @n = n
     end
 
-
+    def factorial
+        if @n < 0
+            puts "Error: Factorial not defined for negative integers"
+          elsif @n == 0
+            return 1
+          else
+            factorial = 1
+            (1..@n).each do |i|
+              factorial *= i
+            end
+            return factorial
+          end
+    end
   
 end
+
+sol = Solver.new(5)
+puts sol.factorial()
