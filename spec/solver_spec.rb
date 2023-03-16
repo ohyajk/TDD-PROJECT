f804c1040a1acb2030231a5ec5a6543b1cb7f3f2
ohyajk <ohyajk@gmail.com>
2023-03-16 17:08:14 +0530

    test return 1 if ero

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -7,4 +7,12 @@ describe Solver do
             expect(sol.factorial).to eql(120)
         end
     end
+
+    context 'should return 1' do
+        solz = Solver.new(0)
+        it 'return 1' do
+            expect(solz.factorial).to eql(1)
+        end
+    end
+
 end
